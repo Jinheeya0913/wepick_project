035fa1c4a888bd554c335dc390c546b1f6a39da8
Jinheeya0913 <taran0913@naver,com>
2023-12-02 17:30:23 +0900

    [2023.12.02] 코드 복사 기능 추가

--- lib/user/view/popup/partner_code_popup.dart
+++ lib/user/view/popup/partner_code_popup.dart
@@ -2,7 +2,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:wepick/common/layout/component/custom_alert_pop.dart';
 import 'package:wepick/user/provider/partner_provider.dart';
 
@@ -19,35 +21,74 @@ class _PartnerCodePopupState extends ConsumerState<PartnerCodePopup> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('파트너 코드 생성'),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('파트너 코드 생성'),
+          IconButton(
+            onPressed: () async {
+              final partnerCode =
+                  await ref.read(partnerProvider.notifier).getMyPartnerCode();
+              // final partnerCode = null;
+              if (partnerCode != null) {
+                setState(() {
+                  code = partnerCode;
+                });
+              } else {
+                showDialog(
+                  context: context,
+                  builder: (_) => CustomSimpleAlertPop(
+                    title: '코드발급 실패',
+                  ),
+                );
+              }
+            },
+            icon: Icon(Icons.refresh_outlined),
+          )
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            context.pop();
+          },
+          child: Text(
+            '닫기',
+          ),
+        ),
+      ],
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(code),
-              IconButton(
-                onPressed: () async {
-                  final partnerCode = await ref
-                      .read(partnerProvider.notifier)
-                      .getMyPartnerCode();
-                  // final partnerCode = null;
-                  if (partnerCode != null) {
-                    setState(() {
-                      code = partnerCode;
-                    });
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (_) => CustomSimpleAlertPop(
-                        title: '코드발급 실패',
-                      ),
-                    );
-                  }
-                },
-                icon: Icon(Icons.refresh_outlined),
-              )
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            copyToClipboard(code);
+                          },
+                          icon: Icon(Icons.copy),
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(code),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ],
@@ -59,5 +100,9 @@ class _PartnerCodePopupState extends ConsumerState<PartnerCodePopup> {
   void initState() {
     super.initState();
     code = '코드를 생성해주세요';
+  }
+
+  void copyToClipboard(text) {
+    Clipboard.setData(ClipboardData(text: text));
   }
 }
