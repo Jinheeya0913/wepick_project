a76fcef2bae744f3eb4e9be0ccf83b06cfaf37e6
Jinheeya0913 <taran0913@naver,com>
2023-12-14 18:32:47 +0900

    [2023.12.14] 요청창 실시간 데이터 반영

---
+++ lib/partner/component/partner_request_card.dart
@@ -0,0 +1,144 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:wepick/common/const/status.dart';
+import 'package:wepick/common/layout/component/custom_circleAvatar.dart';
+import 'package:wepick/common/utils/statusConvertUtil.dart';
+
+import '../../user/model/user_model.dart';
+import '../model/partner_req_que_model.dart';
+
+class PartnerRequestCard extends StatelessWidget {
+  PartnerReqQueModel queModel;
+  UserModel userModel;
+
+  PartnerRequestCard({
+    required this.queModel,
+    required this.userModel,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ptReqStatus = queModel.ptReqStatus;
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            // 임시 이미지
+            CustomCircleAvatar(
+              radius: 80.0,
+              // defaultAssetUrl: 'assets/img/user/my_profile.jpeg',
+              networkImgUrl: userModel.userImgUrl,
+            ),
+            SizedBox(width: 16.0),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '이름',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      userModel.userNm,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '요청일자',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('yyyy-MM-dd').format(queModel!.regDt!),
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '요청처리',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      StatusConvertUtil.statusConvert(ptReqStatus),
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 8.0),
+              ],
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 18.0,
+        ),
+        renderButtonsRow(),
+      ],
+    );
+  }
+
+  Widget renderButtonsRow() {
+    final ptReqStatus = queModel.ptReqStatus;
+    print(queModel.ptTempCd);
+    if (ptReqStatus == STAT_PROGRESSED) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('수락'),
+            ),
+          ),
+          SizedBox(
+            width: 8.0,
+          ),
+          Expanded(
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('거절'),
+            ),
+          ),
+        ],
+      );
+    } else if (ptReqStatus == STAT_COMPLETED) {
+      return Text('완료');
+    } else {
+      return Text('오류');
+    }
+  }
+}
